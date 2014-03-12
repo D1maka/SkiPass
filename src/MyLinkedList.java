83b201427a83c2cffabe10e469321c0df65f0e3e
Dmytro Veres <veres.dima@gmail.com>
2014-03-12 17:48:39 +0200

    Started implementation of linked list

---
+++ src/MyLinkedList.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+
+public class MyLinkedList<T> implements Iterable<T> {
+	
+	private T startNode;
+	private T currentNode;
+	private T 
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+LinkedList 
+}
